cb4fe817af200df8191aefc3f13eb901aae3e601
GranatIT <magnusson.goran@gmail.com>
2019-04-10 21:45:39 +0200

    Convert som old code to js

--- src/csp.js
+++ src/csp.js
@@ -1,4 +1,30 @@
-const stock = [3600, 3300];
-const cuts = {2260: 9, 1190: 2}
-
-// Implement cutting stock problem :-)
+/**
+ * @param  {Array}	master	Inventory pieces, lengths that are available to be cut.
+ * @param  {Array}	cuts 	An array of lengths that were requested.
+ * @param  {Array}	set		A set of cuts pertaining to a given master.
+ */
+function branch(master, cuts, set) {
+	const goods = cuts.filter((cut) => cut < master);
+     //goods = array_filter(cuts, function(v) use (master) { return v <= master;});
+     res = {master, set, cuts};
+     if (goods.length < 1){
+         return res;
+     }
+     remaining = array_diff(cuts, goods);
+     //foreach(goods as k => g){
+     goods.map((g) => {
+         set.push(g);
+         for (i = 0; i < k; i++) {
+         	remaining.push(goods.pop());
+            // array_push(remaining,array_shift(goods));
+         }
+         // array_shift(goods);
+         set = branch(master - g, goods, set);
+         array_walk(remaining, function(k,v) use (set) {array_push(set[2], v);});
+         if (set[0] == 0) return set;
+         if (set[0] < res[0])
+             res = set;
+     })
+     // }
+     return res;
+}

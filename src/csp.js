9b9cf43a79370509db571ca02db4c4a798ae6157
GranatIT <magnusson.goran@gmail.com>
2019-04-11 09:27:16 +0200

    WIP: converting code

--- src/csp.js
+++ src/csp.js
@@ -6,21 +6,22 @@
 function branch(master, cuts, set) {
 	const goods = cuts.filter((cut) => cut < master);
      //goods = array_filter(cuts, function(v) use (master) { return v <= master;});
-     res = {master, set, cuts};
+     res = [...master, ...set, ...cuts];
      if (goods.length < 1){
          return res;
      }
      remaining = array_diff(cuts, goods);
-     //foreach(goods as k => g){
-     goods.map((g) => {
-         set.push(g);
-         for (i = 0; i < k; i++) {
+     //foreach(goods as k => good){
+     goods.map((good, index) => {
+         set.push(good);
+         for (i = 0; i < index; i++) {
          	remaining.push(goods.pop());
             // array_push(remaining,array_shift(goods));
          }
          // array_shift(goods);
-         set = branch(master - g, goods, set);
-         array_walk(remaining, function(k,v) use (set) {array_push(set[2], v);});
+         set = branch(master - good, goods, set);
+         remaining.map(() => {set.push()})
+         array_walk(remaining, function(index,v) use (set) {array_push(set[2], v);});
          if (set[0] == 0) return set;
          if (set[0] < res[0])
              res = set;
